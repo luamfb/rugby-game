a5b1e4f5ac8b893eac59e0088f4f987179362c19
luamfb <luamfb@riseup.net>
2022-03-29 21:15:52 -0300

    defender.c: Implement defender

--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,6 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -12,18 +13,43 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+// extern functions
+// implemented in attacker.c
+extern void initialize_random_seed_if_needed(void);
+
+/*----------------------------------------------------------------------------*/
+/*                             PRIVATE FUNCTIONS                              */
+/*----------------------------------------------------------------------------*/
+
+static direction_t defender_choose_direction(void) {
+  int random_value = rand() % 20;
+  if (random_value == 0) {
+    return (direction_t) DIR_UP;
+  }
+  if (random_value == 1) {
+    return (direction_t) DIR_DOWN;
+  }
+  return (direction_t) DIR_STAY;
+}
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
+
+  // I don't even know the grid size, so I won't bother with spying on
+  // the attacker either
   UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  // This defender is mostly stationary, so even if it hit an obstacle and
+  // couldn't move last turn, that's OK
+  UNUSED(defender_position);
+
+  initialize_random_seed_if_needed();
+
+  return defender_choose_direction();
 }
 
 /*----------------------------------------------------------------------------*/
